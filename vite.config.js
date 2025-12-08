64964dcfaaec41f90f4bbff73659c8f445ca6281
s44158619-code <s88448619@gmail.com>
2025-12-09 04:12:08 +0900

    first

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,16 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    base: '/25-02-PB-assignment-02/', // <--- 이 줄을 꼭 추가하세요!! (본인 레포 이름)
+    plugins: [
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        },
     },
-  },
 })
